2eeaf16cd0f4c4c59750dfec2b42fe0bce4b925c
plyr4 <davelvader@gmail.com>
2026-07-04 14:11:13 -0500

    enhance: revert cname change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://plyr4.com',
+  site: 'https://plyr4.github.io',
+  base: '/site',
 });

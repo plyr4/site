a242dff86f07b50b4e3ffdf8f84bbc55b4e5ac50
plyr4 <davelvader@gmail.com>
2026-07-04 14:00:03 -0500

    enhance: deploy to plyr4.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://plyr4.github.io',
-  base: '/site',
+  site: 'https://plyr4.com',
 });
